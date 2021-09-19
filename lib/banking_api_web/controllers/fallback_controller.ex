1c7369390a3193e0fb2424c115a1e187a51beba1
itsgabriel <gabriel.marques.andrade@gmail.com>
2021-09-19 17:29:07 -0300

    :ok_hand: improve error handlers

--- lib/banking_api_web/controllers/fallback_controller.ex
+++ lib/banking_api_web/controllers/fallback_controller.ex
@@ -12,6 +12,13 @@ defmodule BankingApiWeb.FallbackController do
     |> render("error.json", changeset: changeset)
   end
 
+  def call(conn, %Ecto.Changeset{} = changeset) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(BankingApiWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:bad_request)
@@ -25,5 +32,12 @@ defmodule BankingApiWeb.FallbackController do
     |> put_status(:not_found)
     |> put_view(BankingApiWeb.ErrorView)
     |> render(:"404")
+  end
+
+  def call(conn, {:error, message}) do
+    conn
+    |> put_status(400)
+    |> put_view(BankingApiWeb.ErrorView)
+    |> render("error.json", message: message)
   end
 end
--- lib/banking_api_web/views/error_view.ex
+++ lib/banking_api_web/views/error_view.ex
@@ -13,4 +13,8 @@ defmodule BankingApiWeb.ErrorView do
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
+
+  def render("error.json", %{message: message}) do
+    %{message: message}
+  end
 end
