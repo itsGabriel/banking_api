f8ef7626322a3ff9f14ec749912e01f9d9718dcd
itsgabriel <gabriel.marques.andrade@gmail.com>
2021-09-19 23:42:18 -0300

    :white_check_mark: add useraccounts tests context

--- test/banking_api/user_account_test.exs
+++ test/banking_api/user_account_test.exs
@@ -2,61 +2,33 @@ defmodule BankingApi.UserAccountTest do
   use BankingApi.DataCase
 
   alias BankingApi.UserAccount
+  alias BankingApi.Accounts.User
 
   describe "accounts" do
     alias BankingApi.UserAccount.Account
 
-    @valid_attrs %{}
-    @update_attrs %{}
-    @invalid_attrs %{}
+    @usr_attrs %{email: "testmail.com", password: "some password", username: "some username"}
+    @invalid_attrs %{balance: nil, code: nil, user_id: nil}
 
-    def account_fixture(attrs \\ %{}) do
-      {:ok, account} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> UserAccount.create_account()
-
-      account
+    setup do
+      {:ok, user} = User.changeset(%User{}, @usr_attrs) |> Repo.insert()
+      {:ok, user: user.id}
     end
 
-    test "list_accounts/0 returns all accounts" do
-      account = account_fixture()
-      assert UserAccount.list_accounts() == [account]
+    test "create_account with valid data", state do
+      data = %{code: "2000", balance: 10000, user_id: state[:user]}
+
+      assert {:ok, %Account{} = account} = UserAccount.create_account(data)
     end
 
-    test "get_account!/1 returns the account with given id" do
-      account = account_fixture()
-      assert UserAccount.get_account!(account.id) == account
-    end
-
-    test "create_account/1 with valid data creates a account" do
-      assert {:ok, %Account{} = account} = UserAccount.create_account(@valid_attrs)
-    end
-
-    test "create_account/1 with invalid data returns error changeset" do
+    test "create_account with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = UserAccount.create_account(@invalid_attrs)
     end
 
-    test "update_account/2 with valid data updates the account" do
-      account = account_fixture()
-      assert {:ok, %Account{} = account} = UserAccount.update_account(account, @update_attrs)
-    end
+    test "create_account with invalid balance return error changeset", state do
+      data = %{code: "2000", balance: -1, user_id: state[:user]}
 
-    test "update_account/2 with invalid data returns error changeset" do
-      account = account_fixture()
-      assert {:error, %Ecto.Changeset{}} = UserAccount.update_account(account, @invalid_attrs)
-      assert account == UserAccount.get_account!(account.id)
-    end
-
-    test "delete_account/1 deletes the account" do
-      account = account_fixture()
-      assert {:ok, %Account{}} = UserAccount.delete_account(account)
-      assert_raise Ecto.NoResultsError, fn -> UserAccount.get_account!(account.id) end
-    end
-
-    test "change_account/1 returns a account changeset" do
-      account = account_fixture()
-      assert %Ecto.Changeset{} = UserAccount.change_account(account)
+      assert {:error,  %Ecto.Changeset{}} = UserAccount.create_account(data)
     end
   end
 end
